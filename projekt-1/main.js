2925face65e3b1362e13286169ef2e25ea551fee
Natalia Kędziora <n.m.kedziora@gmail.com>
2017-06-18 14:09:55 +0200

    Project1 Mockup - up&running

--- projekt-1/main.js
+++ projekt-1/main.js
@@ -1,3 +1,16 @@
-document.addEventListener('DOMContentLoaded', function() {
-  console.log('projekt-1');
-});
+class MockupElement extends HTMLElement{
+  constructor() {
+    super();
+
+    this.shadow = this.attachShadow({mode: 'open'});
+  }
+  connectedCallback() {
+    const $template = document.currentScript.ownerDocument.querySelector('template');
+    const $clonedTemplate = $template.cloneNode(true);
+
+    $clonedTemplate.content.querySelector('img').setAttribute('src', this.attributes.image.value);
+    $clonedTemplate.content.querySelector('h1').textContent = this.attributes.title.value;
+    this.shadow.appendChild($clonedTemplate.content);
+  }
+}
+window.customElements.define('mockup-element', MockupElement);
